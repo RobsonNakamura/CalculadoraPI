97db3dcf4f57b280357729b7153798c0d831412b
RobsonNakamura <nakamura.sistemas@gmail.com>
2015-06-21 22:16:28 -0300

    la vai o primeiro commit

---
+++ src/projetocalculadora/ProjetoCalculadoraPI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocalculadora;
+
+import Layout.JFrameCalc;
+
+/**
+ *
+ * @author Robson
+ */
+public class ProjetoCalculadoraPI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrameCalc jFrameCalculadora = new JFrameCalc();
+        jFrameCalculadora.setLocationRelativeTo(null);
+        jFrameCalculadora.setVisible(true);
+    }
+}
