2feacf811b18953110a8e7712dbc7117c4aef070
RobsonNakamura <nakamura.sistemas@gmail.com>
2015-06-22 01:22:24 -0300

    Calculadora

---
+++ src/main/java/com/mycompany/projetocalculadopi/CalculadoraPI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetocalculadopi;
+
+import com.mycompany.calculadorapi.JFrameCalculadora;
+
+/**
+ *
+ * @author Robson
+ */
+public class CalculadoraPI {
+    JFrameCalculadora Calculadora = new JFrameCalculadora();
+    
+}
